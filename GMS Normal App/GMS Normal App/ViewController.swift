99b2d63a56f4054a1b900ac4102ff8280ba1f927
lbakshi <lakshb429@gmail.com>
2021-08-19 23:28:50 -0700

    basic repo setup

---
+++ GMS Normal App/GMS Normal App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GMS Normal App
+//
+//  Created by Lakshya Bakshi on 8/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
