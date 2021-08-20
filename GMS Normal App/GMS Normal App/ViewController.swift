6859811edf1b0457cd526de9146ef22fb1bab064
lbakshi <lakshb429@gmail.com>
2021-08-19 23:48:34 -0700

    primary source code, API key excluded

--- GMS Normal App/GMS Normal App/ViewController.swift
+++ GMS Normal App/GMS Normal App/ViewController.swift
@@ -6,12 +6,26 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
+    
+    let apiKey = "YOUR API KEY"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Code based on Google Maps tutorial at https://developers.google.com/maps/documentation/ios-sdk/map-with-marker?authuser=2
+        // The API key in the tutorial is provided in the AppDelegate file, but since iMessage apps don't have such a file, it is included in a way that is comparable to the iMessage implementation; on the viewDidLoad of the first View Controller
+        GMSServices.provideAPIKey(apiKey)
+        
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        self.view = mapView
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
     }
 
 
